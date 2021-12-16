614efefcfa57ee29ddccdc55027fb5c5cb3070b4
jsbdr <js.bdrx@gmail.com>
2021-12-15 20:59:35 -0600

    null

--- Develop/index.js
+++ Develop/index.js
@@ -1,9 +1,25 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'project name',
+        message: 'What is your project name?',
+        validate: nameInput => {
+            if (nameInput) {
+                return true;
+            }   else {
+                console.log('Please enter your project name!');
+                return false;
+            }
+        }
+    },
+    //ADD MORE MORE MORE MORE MORE MORE MORE MORE MORE
+];
 
-// TODO: Create a function to write README file
+// TODO: Create a function to write README file 
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
